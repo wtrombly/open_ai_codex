1a66ae40bd1c6f2ec6d3d9a3c7e3a6cde555f014
wtrombly <will@therealestateexaminer.com>
2022-12-25 00:04:29 -0600

    fixing minor bugs and typos

--- client/script.js
+++ client/script.js
@@ -22,7 +22,7 @@ function typeText(element, text) {
 
   let interval = setInterval(() => {
     if(index < text.length) {
-      element.innerHTML += text.chartAt(index);
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -75,6 +75,33 @@ chatContainer.scrollTop = chatContainer.scrollHeight;
 const messageDiv = document.getElementById(uniqueId);
 
 loader(messageDiv);
+
+  //fetch data from othe server -> bot's response
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if(response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
 }
 
 form.addEventListener('submit', handleSubmit);
--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config();
 
+console.log(process.env.OPEN_API_KEY)
+
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
